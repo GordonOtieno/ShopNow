6708da8e4e4b9378d0703da17768e1c3e012844c
GordonOtieno <otienogordon95@gmail.com>
2021-05-25 01:33:17 -0700

    initial commit

---
+++ src/data.js
@@ -0,0 +1,47 @@
+const data={
+    products: [
+        {  _id:'1',
+           name:'Nike Slime Shirt',
+           category:'Shirt',
+           image:'./images/p4.jpg',
+           price: 500,
+           brand:'Nike',
+           rating: 4.5,
+           numReview:7,
+           description:'High Quality Shirt'
+       },
+       {  _id:'2',
+       name:'Jeep Slime Shirt',
+       category:'Shirt',
+       image:'./images/p1.jpg',
+       price: 300,
+       brand:'Jeep',
+       rating: 4.5,
+       numReview:4,
+       description:'High Quality Shirt'
+   },
+
+   {  _id:'3',
+   name:'Sweater',
+   category:'sweater',
+   image:'./images/p2.jpg',
+   price: 400,
+   brand:'sweater',
+   rating: 4.5,
+   numReview:10,
+   description:'High Quality sweater'
+}
+, 
+{  _id:'4',
+name:'Boxer',
+category:'Boxer',
+image:'./images/p3.jpg',
+price: 200,
+brand:'Boxer',
+rating: 4.5,
+numReview:11,
+description:'High Quality boxer'
+},
+    ],
+};
+export default data;
