204f5870844a3eb30c52f4d940c1fa54d4b19043
GordonOtieno <otienogordon95@gmail.com>
2023-04-16 23:16:16 +0300

    Update sylings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
@@ -7,12 +7,13 @@ import ProductPage from './pages/ProductPage';
 
 const App = () => {
   return (
-    <div>
+    <div className="grid-container">
       <Router>
       <Navigation />
-        <Route exact path="/" component={HomePage} />
-        <Route path="/product/:id" component={ProductPage} />
-
+      <Routes>
+      <Route path="/product/:id" element={<ProductPage />} />
+      <Route exact path="/" element={<HomePage />} />
+      </Routes>
     </Router>
     </div>
   );
