9a78d6b900e3c4425fb62368c952ef7ada16cbcd
GordonOtieno <otienogordon95@gmail.com>
2023-04-17 21:49:56 +0300

    Create Productslice and userSlice component for react

---
+++ frontend/src/features/ProductSlice.js
@@ -0,0 +1,13 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios  from 'axios';
+
+const initialState = {
+    loading:false,
+    products: [],
+    error: ''
+}
+
+const userSlice = createSlice({
+    
+
+})
